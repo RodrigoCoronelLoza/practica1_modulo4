516f7b6dae71eb5d3f29315ec91aeac04990b2f5
Rodrigo Coronel <drigo31416@gmail.com>
2022-09-06 02:31:13 -0400

    DeleteProductByID added

--- controllers/productController.js
+++ controllers/productController.js
@@ -49,3 +49,25 @@ exports.getProductById = (req, res) => {
     });
   }
 };
+
+exports.deleteProductById = (req, res) => {
+  const products = JSON.parse(
+    fs.readFileSync(`${__dirname}/../data/products.json`)
+  );
+
+  const foundProduct = products.find((p) => p.id == req.params.id);
+  if (foundProduct) {
+    products.splice(products.indexOf(foundProduct),1);
+    fs.writeFileSync(`${__dirname}/../data/products.json`, JSON.stringify(products));
+    res.status(200).json({
+      status: "delete success",
+      data: {
+        product: foundProduct,
+      },
+    });
+  } else {
+    res.status(404).json({
+      status: "not found",
+    });
+  }
+};
